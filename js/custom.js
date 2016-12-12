a3010610a061f932ce98ebf76fc6d4d625dae481
Hasan Khan <HKHAN194@GMAIL.COM>
2016-12-12 02:25:57 +0000

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+
+$(function(){
+	'use strict';
+
+/*--------------------------------------------------
+    Initialize portfolio filter
+    ---------------------------------------------------*/
+	$('#portfolio-grid').mixItUp();
+
+
+	
+/*--------------------------------------------------
+    Page Scroll Features 
+    ---------------------------------------------------*/
+	smoothScroll.init({
+        speed: 1000,
+        updateURL: true,
+        offset: 50
+    });
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 130) {
+            $('#header-nav').addClass('navbar-wooster');
+        } else {
+            $('#header-nav').removeClass('navbar-wooster');
+        }
+    });
+
+});
+
+/*------------------------------------
+Sidenavbar
+-------------------------------*/
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+}
